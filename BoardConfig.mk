9493a491766bf659fc6c290c8044e07693fc87d8
Stricted <info@stricted.net>
2018-01-30 21:26:15 +0100

    remove unneeded init library
    Change-Id: I53f2b98b2c4427da56674af2c5abb3bf7bd6af71

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,8 +20,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-TARGET_INIT_VENDOR_LIB := libinit_s5neolte
-
 # Kernel
 #TARGET_KERNEL_ARCH := arm64
 #TARGET_KERNEL_CONFIG := lineageos_s5neolte_defconfig
